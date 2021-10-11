b970bbb37745219a7157eabb94ca86ac4de7d7f8
emilycatanzariti <ejcatanzariti@loyola.edu>
2021-10-10 21:42:25 -0400

    updated code

--- lab4.py
+++ lab4.py
@@ -5,11 +5,21 @@
 # Program Inputs: package user has, how many gb used, and whether green package user has coupon
 # Program Outputs: cost of phone bill
 
+
+# define phone bill function
+def phone_bill(base_cost, gb_over, base_gb, gb):
+    if gb <= base_gb:
+        return base_cost
+    elif gb > base_gb:
+        return base_cost +((gb - base_gb) * gb_over)
+
+
 # ask user for package
 print("The purpose of this program is to calculate your total phone bill based on what package you have.")
 package = input("What package do you have?")
 package = package.strip().lower()
 
+# assign package to base cost and price for extra gb
 if package == "purple":
     base_cost = 99.99
     gb_over = 0
@@ -25,23 +35,26 @@ elif package == "green":
 else:
     print("Sorry your input is invalid.")
 
-gb = float(input("How many GB did you use this month?"))
-if gb < 0:
-    print("Sorry your input is invalid.")
+# ask user for gb usage to identify any extra costs if applicable
+if package == "green" or package == "orange":
+    gb = round(float((input("How many GB did you use this month?"))))
+    if gb < 0:
+        print("Sorry your input is invalid.")
 
+# calculate total cost
+if package == "green" or package == "orange":
+    total_cost = phone_bill(base_cost, gb_over, base_gb, gb)
+elif pacage == "purple":
+    total_cost = base_cost
 
-def phone_bill(base_cost, gb_over, base_gb, gb):
-    if gb <= base_gb:
-        return base_cost
-    elif gb > base_gb:
-        return base_cost + ((gb - base_gb) * gb_over)
-
-
-total_cost = phone_bill(base_cost, gb_over, base_gb, gb)
-if package == "green":
-    coupon = input("Do you have a coupon yes or no?")
+# ask user if they have coupon, and deduct from total cost if applicable
+if package == "green" and gb >= 0:
+    coupon = input("Do you have a coupon, yes or no?")
     coupon = str(coupon.strip().lower())
     if coupon == "yes":
         if total_cost >= 75:
             total_cost = total_cost - 20
-print("The total cost of your bill is $%.2f" % total_cost)
+
+# output total cost of phone bill
+if (package == "green" or package == "orange" and gb >= 0) or package == "purple":
+    print("The total cost of your bill is $%.2f" % total_cost)
